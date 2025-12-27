432124f6ba5aff1e51bfb165a3b103f5d8b034bd
J0nas7 <jonas.sorensen.93dk@gmail.com>
2025-12-27 21:10:44 +0100

    Stripped scaleway secrets from terraform

--- infra/main.tf
+++ infra/main.tf
@@ -44,8 +44,6 @@ terraform {
 }
 
 provider "scaleway" {
-  access_key         = var.scaleway_access_key
-  secret_key         = var.scaleway_secret_key
   project_id = var.scaleway_project_id
   region             = var.region
   zone               = "fr-par-1"
