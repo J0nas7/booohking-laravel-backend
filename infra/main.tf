31abe7d45448bbb80fb8ab8fc96cb878a3a306ca
J0nas7 <jonas.sorensen.93dk@gmail.com>
2025-12-27 19:26:17 +0100

    Replace default_project_id with project_id.
    Pass correct namespace ID to the output.

--- infra/main.tf
+++ infra/main.tf
@@ -46,6 +46,6 @@ terraform {
 provider "scaleway" {
   access_key         = var.scaleway_access_key
   secret_key         = var.scaleway_secret_key
-  default_project_id = var.scaleway_project_id
+  project_id = var.scaleway_project_id
   region             = var.region
 }
--- infra/modules/containers/outputs.tf
+++ infra/modules/containers/outputs.tf
@@ -3,6 +3,12 @@ output "container_url" {
   value = scaleway_container.laravel_app.domain_name
 }
 
-output "container_id" { value = scaleway_container.laravel_app.id }
-output "namespace_id" { value = data.scaleway_container_namespace.laravel_namespace.id }
-output "image_tag" { value = scaleway_container.laravel_app.registry_image }
+output "container_id" {
+  value = scaleway_container.laravel_app.id
+}
+output "namespace_id" {
+  value = var.namespace_id
+}
+output "image_tag" {
+  value = scaleway_container.laravel_app.registry_image
+}
