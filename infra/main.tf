6b8039036ebc509edaaec53d86e97dc3be159884
J0nas7 <jonas.sorensen.93dk@gmail.com>
2025-12-27 19:09:17 +0100

    Terraform fmt

--- infra/main.tf
+++ infra/main.tf
@@ -4,23 +4,23 @@ data "scaleway_container_namespace" "laravel_namespace" {
 }
 
 module "laravel_app" {
-  source         = "./modules/containers"
-  container_name = var.container_name
-  registry_image = var.registry_image
-  namespace_id   = data.scaleway_container_namespace.laravel_namespace.id
-  app_key        = var.app_key
-  db_url         = var.db_url
-  jwt_secret     = var.jwt_secret
-  mail_password  = var.mail_password
-  mail_host      = var.mail_host
-  mail_port      = "587"
-  mail_username  = "mail@gmail.com"
+  source            = "./modules/containers"
+  container_name    = var.container_name
+  registry_image    = var.registry_image
+  namespace_id      = data.scaleway_container_namespace.laravel_namespace.id
+  app_key           = var.app_key
+  db_url            = var.db_url
+  jwt_secret        = var.jwt_secret
+  mail_password     = var.mail_password
+  mail_host         = var.mail_host
+  mail_port         = "587"
+  mail_username     = "mail@gmail.com"
   mail_from_address = "mail@gmail.com"
   mail_from_name    = "Booohking"
-  session_driver = var.session_driver
-  use_redis      = var.use_redis
-  redis_host     = var.redis_host
-  redis_password = var.redis_password
+  session_driver    = var.session_driver
+  use_redis         = var.use_redis
+  redis_host        = var.redis_host
+  redis_password    = var.redis_password
 
   providers = {
     scaleway = scaleway
@@ -31,7 +31,7 @@ terraform {
   required_providers {
     scaleway = {
       source  = "scaleway/scaleway"
-      version = "~> 2.9"  # pick the latest stable version
+      version = "~> 2.9" # pick the latest stable version
     }
   }
 
@@ -39,8 +39,8 @@ terraform {
 }
 
 provider "scaleway" {
-  access_key  = var.scaleway_access_key
-  secret_key  = var.scaleway_secret_key
+  access_key         = var.scaleway_access_key
+  secret_key         = var.scaleway_secret_key
   default_project_id = var.scaleway_project_id
-  region      = var.region
+  region             = var.region
 }
