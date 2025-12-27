2609e1f8e053b2d9ac4a1805d8f992896f4d0605
J0nas7 <jonas.sorensen.93dk@gmail.com>
2025-12-27 21:17:03 +0100

    Re-placed scaleway secrets in terraform
    SCW_DEBUG: 1

--- infra/main.tf
+++ infra/main.tf
@@ -44,6 +44,8 @@ terraform {
 }
 
 provider "scaleway" {
+  access_key         = var.scaleway_access_key
+  secret_key         = var.scaleway_secret_key
   project_id = var.scaleway_project_id
   region             = var.region
   zone               = "fr-par-1"
